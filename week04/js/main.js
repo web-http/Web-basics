3ab9c706072583594a90d07864b7b6286b1fa837
web-http <20201660@sungshin.ac.kr>
2021-06-13 22:53:19 +0900

    기말고사 과제 제출

---
+++ week04/js/main.js
@@ -0,0 +1,14 @@
+// 햄버거 바 아이콘 선택자
+const menuOpenE1 = document.getElementById('btn--open')
+// 닫는 아이콘 선택자
+const menuCloseEl = document.getElementById('btn--')
+
+menuOpenEl.addEventListner("click", function() {
+    menuOpenEl.style.display="none"
+    menuOpenEl.style.display="block"
+})
+
+menuOpenEl.addEventListner("click", function() {
+    menuOpenEl.style.display="none"
+    menuOpenEl.style.display="block"
+})
---
+++ 과제/js/entry.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },    
+  });
+
+  
